f0a8314a9cad028b10a5ba5eb3b681cf4b19d7dd
vuksavic <vuk.savic@jolacgi.com>
2025-01-09 15:33:48 +0100

    user interfaces!

--- threejs-test-1/script.js
+++ threejs-test-1/script.js
@@ -1,14 +1,27 @@
+import './style.css'
+import gsap from 'gsap'
+import GUI from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-const canvas = document.querySelector('canvas.webgl')
+const gui = new GUI()
 
+const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const geometry = new THREE.SphereGeometry(1, 32, 32)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+gui.add( material, 'wireframe' )
+gui.add( mesh.position, 'y', - 3, 3, 0.01 ).name( 'Elevation' )
+gui
+    .addColor(material, 'color')
+    .name('Color')
+    .onChange((value) => {
+        console.log(material.color + " " + value.getHexString())
+    })
 
 const sizes = {
     width: window.innerWidth,
@@ -25,7 +38,6 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setSize(sizes.width, sizes.height)
 
 const clock = new THREE.Clock()
@@ -37,7 +49,29 @@ window.addEventListener('resize', () => {
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     renderer.setSize(sizes.width, sizes.height)
+})
+
+window.addEventListener('dblclick', () => {
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+    
+    if(!fullscreenElement) {
+        if(canvas.requestFullscreen) {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else {
+        if(document.exitFullscreen) {
+            document.exitFullscreen()
+        }
+        else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+        }
+    }
 })
 
 const tick = () => {
