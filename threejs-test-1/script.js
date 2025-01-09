9a6e9e848a3599a7fc897d2c3af9b513e97c4729
vuksavic <vuk.savic@jolacgi.com>
2025-01-09 14:41:03 +0100

    going big mode

--- threejs-test-1/script.js
+++ threejs-test-1/script.js
@@ -1,58 +1,40 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-console.log(THREE);
+const canvas = document.querySelector('canvas.webgl')
 
-const scene = new THREE.Scene();
-const canvas = document.querySelector('canvas.webgl');
+const scene = new THREE.Scene()
 
-const axesHelper = new THREE.AxesHelper(2);
-
-const group = new THREE.Group();
-group.scale.y = 2;
-group.rotation.y = 0.2;
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube1.position.x = -1.5;
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-cube2.position.x = 0;
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
-);
-cube3.position.x = 1.5;
-
-group.add(cube1);
-group.add(cube2);
-group.add(cube3);
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 const sizes = {
     width: 800,
-    height: 600,
+    height: 600
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(sizes.width, sizes.height)
 
-group.rotation.x = Math.PI * 0.15;
-group.rotation.y = Math.PI * 0.1;
+const clock = new THREE.Clock()
 
-scene.add(group);
-scene.add(camera);
-scene.add(axesHelper);
-renderer.render(scene, camera);
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    controls.update()
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
 
-console.log(group.position.length());
-console.log(group.position.distanceTo(camera.position));
-console.log(group.position.normalize());
+tick()
