45ba777abbdb96c949bc3fc25d400d3b7ef0b541
vuksavic <vuk.savic@jolacgi.com>
2025-01-09 13:27:55 +0100

    transformations and scene graph

--- threejs-test-1/script.js
+++ threejs-test-1/script.js
@@ -4,9 +4,34 @@ console.log(THREE);
 
 const scene = new THREE.Scene();
 const canvas = document.querySelector('canvas.webgl');
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new THREE.Mesh(geometry, material);
+
+const axesHelper = new THREE.AxesHelper(2);
+
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 0.2;
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = 0;
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 1.5;
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
 
 const sizes = {
     width: 800,
@@ -20,6 +45,14 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height);
 
-scene.add(mesh);
+group.rotation.x = Math.PI * 0.15;
+group.rotation.y = Math.PI * 0.1;
+
+scene.add(group);
 scene.add(camera);
+scene.add(axesHelper);
 renderer.render(scene, camera);
+
+console.log(group.position.length());
+console.log(group.position.distanceTo(camera.position));
+console.log(group.position.normalize());
