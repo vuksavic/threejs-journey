895ff4dc12cbb2fd0027a588f6143878d6db81b6
vuksavic <vuk.savic@jolacgi.com>
2025-01-10 12:48:43 +0100

    chapter 1 end

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -1,23 +1,17 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+import GUI from 'lil-gui'
 
-/**
- * Base
- */
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
+const gui = new GUI()
 
-// Scene
 const scene = new THREE.Scene()
 
-/**
- * Sizes
- */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-
 
 const textureLoader = new THREE.TextureLoader()
 
@@ -34,21 +28,48 @@ const gradientTexture = textureLoader.load('./textures/gradients/3.jpg')
 doorColorTexture.colosSpace = THREE.SRGBColorSpace
 matcapTexture.ColorSpace = THREE.SRGBColorSpace
 
-const material = new THREE.MeshBasicMaterial({ map: doorColorTexture})
+// const material = new THREE.MeshBasicMaterial({ map: doorColorTexture, flatShading: false})
+// const material = new THREE.MeshNormalMaterial({ flatShading: true })
+const material = new THREE.MeshPhysicalMaterial({ flatShading: true })
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.1
+// material.roughnessMap = doorRoughnessTexture
+material.roughness = 1
+// material.metalnessMap = doorMetalnessTexture
+material.metalness = 1
+material.shininess = 0.2
+material.transparent = true
+material.alphaMap = doorAlphaTexture
+material.clearcoat = 1
+material.clearcoatRoughness = 0
+material.sheen = 1
+material.sheenRoughness = 0.25
+material.sheenColor.set(1, 1, 1)
+material.iridescence = 1
+material.iridescenceIOR = 1
+material.iridescenceThicknessRange = [ 100, 800 ]
+material.transmission = 1
+material.ior = 1.5
+material.thickness = 0.5
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
+    new THREE.SphereGeometry(0.5, 64, 64),
     material
 )
 sphere.position.x = -1.5
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.PlaneGeometry(1, 1, 100, 100),
     material
 )
 
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128),
     material
 )
 torus.position.x = 1.5
@@ -68,14 +89,51 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+const rgbeLoader = new RGBELoader()
+
+rgbeLoader.load('./textures/environmentMap/2k.hdr', (environmentMap) =>
+    {
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping
+
+        scene.background = environmentMap
+        scene.environment = environmentMap
+
+        console.log(environmentMap)
+    })
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+const pointLight = new THREE.PointLight(0xffffff, 30)
+pointLight.position.set(2, 3, 4)
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
+
+gui.add(material, 'roughness')
+    .min(0).max(1).step(0.0001)
+gui.add(material, 'roughness')
+    .min(0).max(1).step(0.0001)
+gui.add(material, 'clearcoat')
+    .min(0).max(1).step(0.0001)
+gui.add(material, 'clearcoatRoughness')
+    .min(0).max(1).step(0.0001)
+gui.add(material, 'sheen')
+    .min(0).max(1).step(0.0001)
+gui.add(material, 'sheenRoughness')
+    .min(0).max(1).step(0.0001)
+gui.addColor(material, 'sheenColor')
+gui.add(material, 'iridescence')
+    .min(0).max(1).step(0.0001)
+gui.add(material, 'iridescenceIOR')
+    .min(0).max(2.333).step(0.0001)
+gui.add(material.iridescenceThicknessRange, '0')
+    .min(0).max(1000).step(1)
+gui.add(material.iridescenceThicknessRange, '1')
+    .min(0).max(1000).step(1)
+
+// scene.add(ambientLight)
+// scene.add(pointLight)
 scene.add(camera)
 scene.add(sphere, plane, torus)
 
